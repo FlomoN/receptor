c11fd7637c7cf662ca4e63a6f3ccf338a6d8afb5
FlomoN <flo.giger@gmail.com>
2021-03-07 16:44:00 +0100

    updating recipes

--- packages/backend/src/routers/recipe.ts
+++ packages/backend/src/routers/recipe.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 const router = express.Router();
@@ -8,7 +8,7 @@ router.get("/", async (req, res) => {
   try {
     const query = await prisma.recipe.findMany();
     res.json(query);
-  } catch(e){
+  } catch (e) {
     console.log(e);
     res.status(500).end();
   }
@@ -17,34 +17,96 @@ router.get("/", async (req, res) => {
 router.post("/", async (req, res) => {
   const content = req.body;
   try {
-    await prisma.recipe.create({data: content});
+    await prisma.recipe.create({ data: content });
     res.end();
-  } catch (e){
+  } catch (e) {
     console.log(e);
     res.status(500).end();
-  }  
+  }
 });
-
 
 router.delete("/:id", async (req, res) => {
   console.log("deleting recipe", req.params.id);
-  try{
-    await prisma.recipe.delete({where: {id: Number(req.params.id)}});
+  try {
+    await prisma.recipe.delete({ where: { id: Number(req.params.id) } });
     res.end();
-  } catch(e){
+  } catch (e) {
     res.status(404).end();
   }
 });
 
 router.get("/:id", async (req, res) => {
   try {
-    const result = await prisma.recipe.findFirst({where: {id: Number(req.params.id)}});
-    if(result == null){
+    const result = await prisma.recipe.findFirst({
+      where: { id: Number(req.params.id) },
+    });
+    if (result == null) {
       res.status(404).end();
     } else {
       res.json(result);
     }
   } catch (e) {
+    res.status(500).end();
+  }
+});
+
+router.put("/:id", async (req, res) => {
+  type recipeUpdate = {
+    name?: string;
+    content?: string;
+    image?: string;
+    ingredients?: {
+      create: {
+        amount: number;
+        ingredientId: any;
+        unit?: string;
+      }[];
+    };
+  };
+
+  const updateObject: recipeUpdate = req.body;
+
+  if (updateObject.ingredients) {
+    updateObject.ingredients.create = await Promise.all(
+      updateObject.ingredients.create.map(async (elem) => {
+        const matchIngredient = await prisma.ingredient.findFirst({
+          where: { name: elem.ingredientId },
+        });
+        if (matchIngredient == null) {
+          const created = await prisma.ingredient.create({
+            data: {
+              name: elem.ingredientId,
+              unit: elem.unit!,
+            },
+          });
+          return {
+            amount: elem.amount,
+            ingredientId: created.id,
+          };
+        } else {
+          return {
+            amount: elem.amount,
+            ingredientId: matchIngredient.id,
+          };
+        }
+      })
+    );
+
+    console.log("updated ingredients", updateObject.ingredients);
+  }
+
+  try {
+    const result = await prisma.recipe.update({
+      where: { id: Number(req.params.id) },
+      data: updateObject,
+    });
+    if (result == null) {
+      res.status(404).end();
+    } else {
+      res.end();
+    }
+  } catch (e) {
+    console.log(e);
     res.status(500).end();
   }
 });
